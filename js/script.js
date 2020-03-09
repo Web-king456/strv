c50f8c98f920dadbaa6409f164cf6527d937deba
Web-king456 <60809110+Web-king456@users.noreply.github.com>
2020-03-09 21:14:57 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+  $('.first-button').on('click', function () {
+
+    $('.animated-icon1').toggleClass('open');
+  });
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+  $('.third-button').on('click', function () {
+
+    $('.animated-icon3').toggleClass('open');
+  });
+});
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
